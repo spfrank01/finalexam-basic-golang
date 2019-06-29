1da7ebf3fe1da10582b64bef5dacb3dce0f64b2e
phutthinan <phutthinan.s@kbtg.tech>
2019-06-29 14:05:12 +0700

    ""

---
+++ database/database.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	// pq is Postgres database driver
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+//dbURL for global database connection string
+var dbURL string
+
+//InitDB initial database table todo
+func InitDB() {
+	dbURL = os.Getenv("DATABASE_URL")
+	if len(dbURL) == 0 {
+		log.Fatal("Environment variable DATABASE_URL is empty")
+	}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect db", err.Error())
+	}
+	defer db.Close()
+
+	_, err = db.Exec("DROP TABLE todos")
+	if err != nil {
+		log.Fatal("Can't drop table fatal error", err.Error())
+	}
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS cumtomers(
+			id SERIAL PRIMARY KEY,
+			name TEXT,
+			email TEXT,
+			status TEXT
+	);
+	`
+
+	_, err = db.Exec(createTb)
+	if err != nil {
+		log.Fatal("Can't create table fatal error", err.Error())
+	}
+
+}
+
+// Connect open connection to database
+func Connect() (*sql.DB, error) {
+	db, err := sql.Open("postgres", dbURL)
+	return db, err
+}
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main(){
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func main() {
+	r := newPath()
+}
+
+func addMiddleware(c *gin.Context) {
+
+	token := c.GetHeader("Authorization")
+	if token != "token2019" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": http.StatusText(http.StatusUnauthorized)})
+		c.Abort()
+		return
+	}
+
+	c.Next()
+}
+
+func newPath() *gin.Context {
+	r := gin.Default()
+
+	r.Use(addMiddleware)
+
+	r.POST("/customers", service.PostCustomer)
+
+	return r
 }
